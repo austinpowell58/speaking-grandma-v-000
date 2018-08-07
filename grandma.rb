85f951fd050ca6bfebb73da91978a9ac5ba250aa
Austin Powell <austinpowell58@gmail.com>
2018-08-07 02:56:38 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase && phrase != "I LOVE YOU GRANDMA!"
     "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == phrase.upcase
+  elsif phrase == phrase.upcase && phrase != "I LOVE YOU GRANDMA!"
     "NO, NOT SINCE 1938!" 
   elsif phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
