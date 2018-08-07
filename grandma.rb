07cfee3effc2fd23bdb0d4a94e736d6c7adcfbf1
Austin Powell <austinpowell58@gmail.com>
2018-08-07 02:46:03 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,7 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  if phrase == phrase.upcas
+  if phrase == phrase.upcase
+    puts "No, NOT SINCE 1938!"
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
